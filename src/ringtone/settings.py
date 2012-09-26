9b85b0fe9539f534f5127421f4e6ad462d42f849
QiXiao <QXiao@bainainfo.com>
2012-09-26 20:19:49 +0800

    Disabled some system apps: admin doc, site.

--- src/ringtone/settings.py
+++ src/ringtone/settings.py
@@ -107,11 +107,11 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
     'ringtone.services',
     'ringtone.mobosite',
 )
